0920d1de6ae50250ca0102405ff9c1c4705d156c
MirageDev <forpixel.studio@gmail.com>
2023-11-23 21:20:14 +0100

    fix H files (i fucking hate C++)

--- CameraCalibration.h
+++ CameraCalibration.h
@@ -8,11 +8,17 @@
 #include <stdio.h>
 #include <vector>
 
-std::vector<cv::Point3f> CAM_CALIB_OBJ_POINT;
-std::vector<std::vector<cv::Point3f>> CAM_CALIB_OBJ_POINTS;
-std::vector<std::vector<cv::Point2f>> CAM_CALIB_IMG_POINTS;
+#ifndef VRCAMCALIB_H
+#define VRCAMCALIB_H
 
-void startCalibratingCamera(float squareSize) {
+// A4 paper is (10.5, 14.85)
+extern std::vector<cv::Point3f> CALIBRATION_MAT_OBJECT;
+
+inline std::vector<cv::Point3f> CAM_CALIB_OBJ_POINT;
+inline std::vector<std::vector<cv::Point3f>> CAM_CALIB_OBJ_POINTS;
+inline std::vector<std::vector<cv::Point2f>> CAM_CALIB_IMG_POINTS;
+
+inline void startCalibratingCamera(float squareSize) {
 	CAM_CALIB_OBJ_POINT.clear();
 	CAM_CALIB_OBJ_POINTS.clear();
 	CAM_CALIB_IMG_POINTS.clear();
@@ -24,7 +30,7 @@ void startCalibratingCamera(float squareSize) {
 	}
 }
 
-std::tuple<bool, cv::Mat, cv::Mat> calibrateCamera(cv::Mat image, bool finish) {
+inline std::tuple<bool, cv::Mat, cv::Mat> calibrateCamera(cv::Mat image, bool finish) {
 	if (!finish) {
 		cv::TermCriteria criteria = cv::TermCriteria(cv::TermCriteria::EPS | cv::TermCriteria::MAX_ITER, 30, 0.001);
 
@@ -58,3 +64,24 @@ std::tuple<bool, cv::Mat, cv::Mat> calibrateCamera(cv::Mat image, bool finish) {
 		return { true, cv::Mat(), cv::Mat() };
 	}
 }
+
+inline std::tuple<bool, cv::Mat> calibrateWorldMatrix(std::vector<cv::Point2f> samples, cv::Mat cameraMatrix, cv::Mat cameraDistortion) {
+	CALIBRATION_MAT_OBJECT = {
+		cv::Point3f(-10.5f, 0.f, -14.85f),
+		cv::Point3f(10.5f, 0.f, 14.85f),
+		cv::Point3f(-10.5f, 0.f, 14.85f),
+		cv::Point3f(10.5f, 0.f, -14.85f)
+	};
+
+	cv::Mat rvecs, tvecs;
+	bool ret = cv::solvePnP(CALIBRATION_MAT_OBJECT, samples, cameraMatrix, cameraDistortion, rvecs, tvecs);
+
+	if (ret) {
+		std::cout << "Computing inverse camera pose..." << std::endl;
+
+	}
+	else {
+		return { false, cv::Mat() };
+	}
+}
+#endif
